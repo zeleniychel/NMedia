dd94aefedef33df23f7a2f9e65e17493122e59fe
Anton Kamnev AN44 <gish18@gmail.com>
2023-08-04 23:30:17 +0300

    1

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,10 +1,15 @@
 package ru.netology.nmedia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.nmedia.adapter.OnInteractionListener
 import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.util.AndroidUtils
+import ru.netology.nmedia.util.AndroidUtils.focusAndShowKeyboard
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -14,15 +19,45 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val viewModel: PostViewModel by viewModels()
-        val adapter = PostsAdapter({
-            viewModel.likeById(it.id)
-        }, {
-            viewModel.share(it.id)
-        }
-        )
+        val adapter = PostsAdapter(object : OnInteractionListener {
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+            }
+
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+        })
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
-            adapter.submitList(posts)
+            adapter.submitList(posts) {
+                binding.list.smoothScrollToPosition(0)
+            }
+
+        }
+
+        viewModel.edited.observe(this) { post ->
+            if (post.id !== 0L) {
+                binding.content.setText(post.content)
+                binding.content.focusAndShowKeyboard()
+            }
+        }
+
+        binding.save.setOnClickListener {
+            val text = binding.content.text.toString()
+            if (text.isEmpty()) {
+                Toast.makeText(this, R.string.error_empty_content, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            viewModel.changeContentAndSave(text)
+
+            binding.content.setText("")
+            binding.content.clearFocus()
+            AndroidUtils.hideKeyboard(it)
         }
     }
 }
--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -3,8 +3,10 @@ package ru.netology.nmedia.repository
 import androidx.lifecycle.LiveData
 import ru.netology.nmedia.dto.Post
 
-interface PostIRepository {
+interface PostRepository {
     fun getAll(): LiveData<List<Post>>
     fun likeById(id: Long)
     fun share(id: Long)
+    fun removeById(id:Long)
+    fun save (post: Post)
 }
